f21980b0d88310ecadc7e33f313f7a53e56ad161
Dmitry Mozzherin <dmozzherin@gmail.com>
2021-09-21 15:34:54 +0000

    documentation (close #3)

--- interface.go
+++ interface.go
@@ -1,10 +1,17 @@
+/*
+ */
 package aho_corasick
 
 import "github.com/gnames/aho_corasick/ent/match"
 
+// AcoCorasick
 type AhoCorasick interface {
+	// Setup is
 	Setup(patterns []string) int
+	// Search is
 	Search(haystack string) []match.Match
+	// SearchUniq
 	SearchUniq(haystack string) []match.Match
+	// Debug
 	Debug(haystack string)
 }
